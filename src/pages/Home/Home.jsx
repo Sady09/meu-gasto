128728f790c99123a7a231f939719f927dfd6ee2
João Pedro Sady <joaosady667@gmail.com>
2024-06-20 12:25:48 -0300

    v1.0.0 - Estrutura inicial do projeto

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 function Home() {
-  return <div></div>;
+  return (
+    <div className="flex gap-4">
+      <div className="flex flex-1 bg-red-700">
+        {/* Primeiro quadrado */}
+        <div className="flex"></div>
+        {/* Segundo quadrado com 2 quadradinhos */}
+        <div className="flex">
+          <div className="flex"></div>
+          <div className="flex"></div>
+        </div>
+        {/* Ultimo quadrado */}
+        <div className="flex"></div>
+      </div>
+      <div className="flex flex-[2_2_0%] bg-blue-700"></div>
+    </div>
+  );
 }
 
 export default Home;
